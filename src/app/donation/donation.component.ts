b51bdb79dd88bcc6820d0f74c52f4c489aaaaef2
J-Jamet <jeremy.jamet@kunzisoft.com>
2021-08-15 14:49:41 +0200

    Update donations

--- src/app/donation/donation.component.ts
+++ src/app/donation/donation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { faBitcoin, faEthereum, faGooglePlay, faMonero, faPaypal, IconDefinition } from '@fortawesome/free-brands-svg-icons';
-import { faCoffee, faCreditCard, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faBitcoin, faEthereum, faGooglePlay, faMonero, faPaypal } from '@fortawesome/free-brands-svg-icons';
+import { faCoffee, faCertificate, faCreditCard, faHeart } from '@fortawesome/free-solid-svg-icons';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -14,6 +14,7 @@ export class DonationComponent implements OnInit {
   faCoffee = faCoffee
   faBitcoin = faBitcoin
   faEthereum = faEthereum
+  faCardano = faCertificate
   faMonero = faMonero
   faGooglePlay = faGooglePlay
   faPaypal = faPaypal
@@ -22,27 +23,43 @@ export class DonationComponent implements OnInit {
   cryptoCurrencies = [
     {
       name: "Bitcoin",
-      icon: faBitcoin,
+      icon: this.faBitcoin,
       webSiteUrl: "https://bitcoin.org/",
       qrImgPath: "assets/img/bitcoin_qr.png",
       walletAddress: "1DSwXCk7Sob24sNsofywNoRQw2f5Qj5t2F",
-      walletUrl: "bitcoin:1DSwXCk7Sob24sNsofywNoRQw2f5Qj5t2F"
+      walletUrl: "bitcoin:1DSwXCk7Sob24sNsofywNoRQw2f5Qj5t2F",
+      explorer: "https://blockstream.info/address/1DSwXCk7Sob24sNsofywNoRQw2f5Qj5t2F",
+      highlighted: false
     },
     {
       name: "Ethereum",
-      icon: faEthereum,
+      icon: this.faEthereum,
       webSiteUrl: "https://www.ethereum.org/",
       qrImgPath: "assets/img/ethereum_qr.png",
       walletAddress: "0x6F5De8c22C4869c4a5D3a48975a42C9498de3CAB",
-      walletUrl: "ethereum:0x6F5De8c22C4869c4a5D3a48975a42C9498de3CAB"
+      walletUrl: "ethereum:0x6F5De8c22C4869c4a5D3a48975a42C9498de3CAB",
+      explorer: "https://etherchain.org/account/6F5De8c22C4869c4a5D3a48975a42C9498de3CAB",
+      highlighted: true
+    },
+    {
+      name: "Cardano",
+      icon: this.faCardano,
+      webSiteUrl: "https://cardano.org/",
+      qrImgPath: "assets/img/cardano_qr.png",
+      walletAddress: "addr1qxfuczz53wuwa4dqmluptakv2gnkgslqyhf73spq2vvkykcu0tkh8aw9ehfd8xfqdtp6vvz0nnz6xzkltzfxua5uv35qqzm4s0",
+      walletUrl: "cardano:addr1qxfuczz53wuwa4dqmluptakv2gnkgslqyhf73spq2vvkykcu0tkh8aw9ehfd8xfqdtp6vvz0nnz6xzkltzfxua5uv35qqzm4s0",
+      explorer: "https://explorer.cardano.org/en/address.html?address=addr1qxfuczz53wuwa4dqmluptakv2gnkgslqyhf73spq2vvkykcu0tkh8aw9ehfd8xfqdtp6vvz0nnz6xzkltzfxua5uv35qqzm4s0",
+      highlighted: false
     },
     {
       name: "Monero",
-      icon: faMonero,
+      icon: this.faMonero,
       webSiteUrl: "https://getmonero.org/",
       qrImgPath: "assets/img/monero_qr.png",
       walletAddress: "4BFGwyshAa2YwwXNboQ4r78Vv9hf83cFBCF8vAd8jAQRbUQho187hKSLQpzWBsV7LW2gNXUthvb8W4hHBifTfhdSMKvTDP7",
-      walletUrl: "monero:4BFGwyshAa2YwwXNboQ4r78Vv9hf83cFBCF8vAd8jAQRbUQho187hKSLQpzWBsV7LW2gNXUthvb8W4hHBifTfhdSMKvTDP7"
+      walletUrl: "monero:4BFGwyshAa2YwwXNboQ4r78Vv9hf83cFBCF8vAd8jAQRbUQho187hKSLQpzWBsV7LW2gNXUthvb8W4hHBifTfhdSMKvTDP7",
+      explorer: "",
+      highlighted: false
     }
   ]
 
