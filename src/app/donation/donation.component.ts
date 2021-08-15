288b0fa7160f06008639341410a585cee8ce4a68
J-Jamet <jeremy.jamet@kunzisoft.com>
2021-08-15 22:05:53 +0200

    Manual copy of currency

--- src/app/donation/donation.component.ts
+++ src/app/donation/donation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faBitcoin, faEthereum, faGooglePlay, faMonero, faPaypal } from '@fortawesome/free-brands-svg-icons';
-import { faCoffee, faCertificate, faCreditCard, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faCopy, faCoffee, faCertificate, faCreditCard, faHeart } from '@fortawesome/free-solid-svg-icons';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -19,6 +19,7 @@ export class DonationComponent implements OnInit {
   faGooglePlay = faGooglePlay
   faPaypal = faPaypal
   faCreditCard = faCreditCard
+  faCopy = faCopy
 
   cryptoCurrencies = [
     {
@@ -28,7 +29,6 @@ export class DonationComponent implements OnInit {
       webSiteUrl: "https://bitcoin.org/",
       qrImgPath: "assets/img/bitcoin_qr.png",
       walletAddress: "1DSwXCk7Sob24sNsofywNoRQw2f5Qj5t2F",
-      walletUrl: "bitcoin:1DSwXCk7Sob24sNsofywNoRQw2f5Qj5t2F",
       explorer: "https://blockstream.info/address/1DSwXCk7Sob24sNsofywNoRQw2f5Qj5t2F",
       highlighted: false
     },
@@ -39,7 +39,6 @@ export class DonationComponent implements OnInit {
       webSiteUrl: "https://www.ethereum.org/",
       qrImgPath: "assets/img/ethereum_qr.png",
       walletAddress: "0x6F5De8c22C4869c4a5D3a48975a42C9498de3CAB",
-      walletUrl: "ethereum:0x6F5De8c22C4869c4a5D3a48975a42C9498de3CAB",
       explorer: "https://etherchain.org/account/6F5De8c22C4869c4a5D3a48975a42C9498de3CAB",
       highlighted: true
     },
@@ -50,7 +49,6 @@ export class DonationComponent implements OnInit {
       webSiteUrl: "https://cardano.org/",
       qrImgPath: "assets/img/cardano_qr.png",
       walletAddress: "addr1qxfuczz53wuwa4dqmluptakv2gnkgslqyhf73spq2vvkykcu0tkh8aw9ehfd8xfqdtp6vvz0nnz6xzkltzfxua5uv35qqzm4s0",
-      walletUrl: "cardano:addr1qxfuczz53wuwa4dqmluptakv2gnkgslqyhf73spq2vvkykcu0tkh8aw9ehfd8xfqdtp6vvz0nnz6xzkltzfxua5uv35qqzm4s0",
       explorer: "https://explorer.cardano.org/en/address.html?address=addr1qxfuczz53wuwa4dqmluptakv2gnkgslqyhf73spq2vvkykcu0tkh8aw9ehfd8xfqdtp6vvz0nnz6xzkltzfxua5uv35qqzm4s0",
       highlighted: false
     },
@@ -61,7 +59,6 @@ export class DonationComponent implements OnInit {
       webSiteUrl: "https://getmonero.org/",
       qrImgPath: "assets/img/monero_qr.png",
       walletAddress: "4BFGwyshAa2YwwXNboQ4r78Vv9hf83cFBCF8vAd8jAQRbUQho187hKSLQpzWBsV7LW2gNXUthvb8W4hHBifTfhdSMKvTDP7",
-      walletUrl: "monero:4BFGwyshAa2YwwXNboQ4r78Vv9hf83cFBCF8vAd8jAQRbUQho187hKSLQpzWBsV7LW2gNXUthvb8W4hHBifTfhdSMKvTDP7",
       explorer: "",
       highlighted: false
     }
@@ -70,16 +67,28 @@ export class DonationComponent implements OnInit {
   constructor(private modalService: NgbModal) {
     
   }
+
   ngOnInit(): void {
   
   }
 
   openCryptoCurrencyModal(content: any) {
       this.modalService.open(content).result.then((res) => {
-
       }, (res) => {
-
     });
+  }
+
+  copyCurrencyToClipboard(content: any) {
+    navigator.clipboard.writeText(content.walletAddress).then(function() {
+      console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+    /*
+    textNode.select()
+    document.execCommand("copy");
+    textNode.remove()
+    */
   }
 
 }
